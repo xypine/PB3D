c0b600a8f3fbc4d85344236c89d39ae65c32cff7
jonnelafin <elias.eelinen@gmail.com>
2019-11-28 18:43:55 +0200

    added rotation matricies (possible glitches)

--- src/codenameprojection/CodeNameProjection.java
+++ src/codenameprojection/CodeNameProjection.java
@@ -19,7 +19,7 @@ import JFUtils.fVector3;
  * @author Elias Eskelinen
  */
 public class CodeNameProjection {
-    public static double minUtilsVer = 2.2;
+    public static double minUtilsVer = 2.21;
 
     /**
      * @param args the command line arguments
@@ -40,7 +40,9 @@ class driver{
     private int xScreenCenter = 320/2;
     private int yScreenCenter = 240/2;
     private dVector3 screenPosition = new dVector3( 0, 0, 7 );
+    private dVector3 screenPosition_org = screenPosition.clone();
     private dVector3 viewAngle = new dVector3( 0, 90, 90 );
+    private dVector3 viewAngle_org = viewAngle.clone();
     
     private static final double DEG_TO_RAD = 0.017453292;
     private double modelScale = 10;
@@ -49,6 +51,8 @@ class driver{
     double ST = Math.sin( DEG_TO_RAD * viewAngle.x );
     double CP = Math.cos( DEG_TO_RAD * viewAngle.y );
     double SP = Math.sin( DEG_TO_RAD * viewAngle.y );
+    
+    
     
     public void addCube(dVector3 center, double size){
         double s = size;
@@ -97,8 +101,10 @@ class driver{
         lines = new LinkedList<>();
         addCube(new dVector3(0, 0, 0), 1);
         
-        float angleK = 0;
+        double angleK = 0;
         double angleM = 0;
+        double angleX = 0;
+        double angleXM = 0;
         int sleep = 1;
         while(true){
             //Init
@@ -110,54 +116,86 @@ class driver{
             xScreenCenter = s.r.w / 2;
             yScreenCenter = s.r.h / 2;
             
-            //Check input
+            //Check input   -0.025D*0.05
             double factor = -0.025D*0.05;
             if(inp.keys[68] == true){
-                screenPosition.x += factor;
+                screenPosition_org.x += factor;
             }
             if(inp.keys[65] == true){
-                screenPosition.x -= factor;
+                screenPosition_org.x -= factor;
             }
             if(inp.keys[87] == true){
-                screenPosition.y += factor;
+                screenPosition_org.y += factor;
             }
             if(inp.keys[83] == true){
-                screenPosition.y -= factor;
+                screenPosition_org.y -= factor;
             }
             if(inp.keys[81] == true){
-                screenPosition.z -= factor*5;
+                screenPosition_org.z -= factor*5;
             }
             if(inp.keys[69] == true){
-                screenPosition.z += factor*5;
+                screenPosition_org.z += factor*5;
             }
-            //l
-            if(inp.keys[76] == true){
+            //c
+            if(inp.keys[67] == true){
                 viewAngle.z += factor*15;
+            }
+            //z
+            if(inp.keys[90] == true){
+                viewAngle.z -= factor*15;
             }
             //j
             if(inp.keys[74] == true){
-                viewAngle.z -= factor*15;
+                angleXM = angleXM + 0.0001D;
+            }
+            //l
+            if(inp.keys[76] == true){
+                angleXM = angleXM - 0.0001D;
             }
             //i
             if(inp.keys[73] == true){
-                angleM = angleM + 0.001D;
+                angleM = angleM + 0.0001D;
             }
             //k
             if(inp.keys[75] == true){
-                angleM = angleM - 0.001D;
+                angleM = angleM - 0.0001D;
+            }
+            if(inp.keys[86] == true){
+                inp.verbodose = !inp.verbodose;
             }
             else{
             }
             
+            screenPosition = screenPosition_org;
+            screenPosition = matmul(RX((float) angleM), screenPosition_org.toFVector3()).toDVector3();
+            //screenPosition.z = screenPosition_org.z;
             //Calc
             for(dVector3 i : points){
                 //System.out.println("Original[" +i.hashCode() + "] :" + i);
                 
-                fVector3 rotated = matmul(RX(angleK), i.toFVector3());
-                rotated = matmul(RZ(0), rotated);
-                rotated = matmul(RY(-0.06F), rotated);
+                fVector3 rotated = matmul(RX((float) angleK ), i.toFVector3());
+                rotated = matmul(RY((float) angleX ), rotated);
+                /*rotated = matmul(RX((float) 0 ), rotated);
+                rotated = matmul(RX((float) 0 ), rotated);
+                rotated = matmul(RX((float) 0 ), rotated);*/
+                //rotated.z = (float) i.z;
+                //fVector3 rotated = i.toFVector3();
+                //rotated = matmul(RZ(0), rotated);
+                //rotated = matmul(RY(-0.06F), rotated);
+                
+                float dist = 5F;
+                float z = 1 / (dist - rotated.z);
+                
                 dVector3 projected = new dVector3(0, 0, 0);
+                /*float[][] projection = {
+                {z, 0, 0},
+                {0, z, 0}
+                };
+                dVector3 projected = new dVector3(0, 0, 0);*/
                 projectPoint(rotated.toDVector3(), projected);
+                /*projected = matmul(projection, rotated).toDVector3();
+                projected = dVector3.multiply(projected, new dVector3(200, 200, 200));
+                projected = dVector3.add(projected, new dVector3(200, 200, 20));*/
                 dVector point2D = new dVector(projected.x, projected.y);
                 point2D.identifier = i.identifier;
                 int size = (int) (25 - (screenPosition.z - rotated.z) * 2);
@@ -165,8 +203,10 @@ class driver{
                     size = 0;
                 }
                 //System.out.println("Projected: " + point2);
-                sizes.add(new dVector(size, size));
-                set.add(point2D);
+                if(rotated.z < screenPosition.z){
+                    sizes.add(new dVector(size, size));
+                    set.add(point2D);
+                }
             }
             /*for(Integer[] l : lines){
             //Every point must exist in both lists
@@ -187,6 +227,8 @@ class driver{
             
             angleM = angleM * 0.95D;
             angleK = (float) (angleK + angleM);
+            angleXM = angleXM * 0.95D;
+            angleX = (float) (angleX + angleXM);
             try {
                 Thread.sleep(sleep);
             } catch (InterruptedException ex) {
