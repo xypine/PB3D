ed96e6db71749c07f5219ca975680ab99e7f9632
Elias Eskelinen <elias.eskelinen@protonmail.com>
2020-01-12 17:27:44 +0200

    added animation support for the blender exporter

--- tools/blender_modelExporter.py
+++ tools/blender_modelExporter.py
@@ -3,36 +3,81 @@
 #Open and run this file inside blender's text editor
 import bpy, bmesh
 obj = bpy.context.active_object
+scn = bpy.context.scene
 
-if obj.mode == 'EDIT':
-    # this works only in edit mode,
-    bm = bmesh.from_edit_mesh(obj.data)
-    verts = [vert.co for vert in bm.verts]
-    lines = [edge.vertices for edge in bm.edges]
-    faces = [face.vertices for edge in bm.polygons]
+frame = bpy.context.scene.frame_current
+end = scn.frame_end
+frames = range(end)
 
-else:
-    # this works only in object mode,
-    verts = [vert.co for vert in obj.data.vertices]
-    lines = [edge.vertices for edge in obj.data.edges]
-    faces = [edge.vertices for edge in obj.data.polygons]
-# coordinates as tuples
-plain_verts = [vert.to_tuple() for vert in verts]
+#Flags
+keyframes = True
 
+if not keyframes:
+    frames = [frame]
+verts = []
+lines = []
+faces = []
 
+def func_object_duplicate_flatten_modifiers(context, ob):
+    depth = bpy.context.evaluated_depsgraph_get()
+    eobj = ob.evaluated_get(depth)
+    mesh = bpy.data.meshes.new_from_object(eobj)
+    name = ob.name + "_clean"
+    new_object = bpy.data.objects.new(name, mesh)
+    new_object.data = mesh
+    bpy.context.collection.objects.link(new_object)
+    return new_object
 
+def updateMesh():
+    global verts
+    global lines
+    global faces
+    
+    #new_obj = obj.copy()
+    #new_obj.data = obj.data.copy()
+    #new_obj.animation_data_clear()
+    #bpy.data.scenes[0].objects.link(new_obj)
+    
+    
+    new_obj = func_object_duplicate_flatten_modifiers(bpy.context, obj)
+    
+    if obj.mode == 'EDIT':
+        # this works only in edit mode,
+        bm = bmesh.from_edit_mesh(new_obj.data)
+        verts = [vert.co for vert in bm.verts]
+        lines = [edge.vertices for edge in bm.edges]
+        faces = [face.vertices for edge in bm.polygons]
+    
+    else:
+        # this works only in object mode,
+        verts = [vert.co for vert in new_obj.data.vertices]
+        lines = [edge.vertices for edge in new_obj.data.edges]
+        faces = [edge.vertices for edge in new_obj.data.polygons]
+    # coordinates as tuples
+    plain_verts = [vert.to_tuple() for vert in verts]
+    #Delete the new_obj
+    bpy.ops.object.select_all(action='DESELECT')
+    new_obj.select_set(True) # Blender 2.8x
+    bpy.ops.object.delete() 
+#deselectA()
+
+updateMesh()
 #print(plain_verts)
 st = ""
 st2 = ""
 st3 = ""
 print("Reading vertices...",end="")
-for i in verts:
-    for c in i:
-        #print(int(c*100),end=" ")
-        st = st + str(int(c*100)) + " "
-    #print()
+for t in frames:
+    bpy.context.scene.frame_set(t)
+    updateMesh()
+    st = st + "\n#" + str(t) + "\n"
+    for i in verts:
+        for c in i:
+            #print(int(c*100),end=" ")
+            st = st + str(int(c*100)) + " "
+        #print()
+        st = st + "\n"
     st = st + "\n"
-st = st + "\n"
 print("DONE")
 print("Reading edges...",end="")
 for i in lines:
@@ -61,3 +106,6 @@ f.from_string(st2)
 f = bpy.data.texts.new('model_faces.txt')
 f.from_string(st3)
 print("Everything done, have a good day.")
+
+
+
