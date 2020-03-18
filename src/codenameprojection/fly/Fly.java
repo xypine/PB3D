21510adfb3f1f6f4b2c8d9211893f4d8e2ce34da
jonnelafin <elias.eelinen@gmail.com>
2020-03-18 19:44:22 +0200

    fixed some major bugs with rotating

--- src/codenameprojection/fly/Fly.java
+++ src/codenameprojection/fly/Fly.java
@@ -136,25 +136,28 @@ public class Fly {
         shipM.frames.get(0).points.add(c3);
         shipM.frames.get(0).points.add(c4);
         
-        System.out.println(shipM.getFrame(0).points.indexOf(c1));
+        System.out.println(shipM.frames.get(0).points.indexOf(c1));
         
         
         //float shipRotX = 0;
         while (true) {
             shipM = Driver.models.get(ship);
-            if(shipM.getFrame(0).points.indexOf(c1) == -1){
-            //    shipM.getFrame(0).points.add(c1);
+            if(shipM.frames.get(0).points.indexOf(c1) == -1){
+                shipM.frames.get(0).points.add(c1);
             }
-            if(shipM.getFrame(0).points.indexOf(c2) == -1){
-            //    shipM.getFrame(0).points.add(c2);
+            if(shipM.frames.get(0).points.indexOf(c2) == -1){
+                shipM.frames.get(0).points.add(c2);
             }
-            if(shipM.getFrame(0).points.indexOf(c3) == -1){
-            //    shipM.getFrame(0).points.add(c3);
+            if(shipM.frames.get(0).points.indexOf(c3) == -1){
+                shipM.frames.get(0).points.add(c3);
             }
-            if(shipM.getFrame(0).points.indexOf(c4) == -1){
-            //    shipM.getFrame(0).points.add(c4);
+            if(shipM.frames.get(0).points.indexOf(c4) == -1){
+                shipM.frames.get(0).points.add(c4);
             }
-            
+            c1i = shipM.frames.get(0).points.indexOf(c1);
+            c2i = shipM.frames.get(0).points.indexOf(c2);
+            c3i = shipM.frames.get(0).points.indexOf(c3);
+            c4i = shipM.frames.get(0).points.indexOf(c4);
             //shipM = Driver.models.get(ship);
             Driver.angleYM = Driver.angleYM + Driver.inp.cX * 0.0002;
             Driver.inp.cX = (int) (Driver.inp.cX * 0.5);
@@ -164,27 +167,40 @@ public class Fly {
             model shipModel = Driver.models.get(ship);
             Point3D shipCenter = Utils.average(shipModel.getFrame(0).points);
             //System.out.println(Driver.inp.mouseX());
+            LinkedList<Integer> torem = new LinkedList<>();
             for(Integer bolt : boltHandles){
-                model cursor = Driver.models.get(bolt);
-                //Point3D.multiply(Driver.viewAngle, new Point3D(1, 1, 1)
-                double speeds = 0.5;
-                Point3D one = cursor.getFrame(0).points.get(0);
-                Point3D two = cursor.getFrame(0).points.get(1);
-                //cursor.getFrame(0).points.get(0).z = cursor.getFrame(0).points.get(0).z - 0.5;
-                //cursor.getFrame(0).points.get(1).z = cursor.getFrame(0).points.get(1).z - 0.5;
-                Point3D newPo = Point3D.add(one, Point3D.subtract(one, two));
-                Point3D newPt = Point3D.add(two, Point3D.subtract(one, two));
-                Point3D velBolt = Point3D.subtract(one, two);
-                cursor.x = cursor.x + velBolt.x;
-                cursor.y = cursor.y + velBolt.y;
-                cursor.z = cursor.z + velBolt.z;
-                //cursor.getFrame(0).points.get(0).x = newPo.x;
-                //cursor.getFrame(0).points.get(0).y = newPo.y;
-                //cursor.getFrame(0).points.get(0).z = newPo.z;
-                
-                //cursor.getFrame(0).points.get(1).x = newPt.x;
-                //cursor.getFrame(0).points.get(1).y = newPt.y;
-               // cursor.getFrame(0).points.get(1).z = newPt.z;
+                try {
+                    model cursor = Driver.models.get(bolt);
+                    //Point3D.multiply(Driver.viewAngle, new Point3D(1, 1, 1)
+                    double speeds = 0.5;
+                    Point3D one = cursor.frames.get(0).points.get(0);
+                    Point3D two = cursor.frames.get(0).points.get(1);
+                    //cursor.getFrame(0).points.get(0).z = cursor.getFrame(0).points.get(0).z - 0.5;
+                    //cursor.getFrame(0).points.get(1).z = cursor.getFrame(0).points.get(1).z - 0.5;
+                    Point3D newPo = Point3D.add(one, Point3D.subtract(one, two));
+                    Point3D newPt = Point3D.add(two, Point3D.subtract(one, two));
+                    Point3D velBolt = Point3D.subtract(one, two);
+                    cursor.x = cursor.x + velBolt.x;
+                    cursor.y = cursor.y + velBolt.y;
+                    cursor.z = cursor.z + velBolt.z;
+                    if (Math.abs(Utils.getDistance(new Point3D(cursor.x, cursor.y, cursor.z), new Point3D(0, 0, 0))) > 2000) {
+                        //Driver.models.remove(bolt);
+                        torem.add(bolt);
+                    } else {
+                        //System.out.println(Math.abs(Utils.getDistance(new Point3D(cursor.x, cursor.y, cursor.z), new Point3D(0, 0, 0))));
+                    }
+                    //cursor.getFrame(0).points.get(0).x = newPo.x;
+                    //cursor.getFrame(0).points.get(0).y = newPo.y;
+                    //cursor.getFrame(0).points.get(0).z = newPo.z;
+
+                    //cursor.getFrame(0).points.get(1).x = newPt.x;
+                    //cursor.getFrame(0).points.get(1).y = newPt.y;
+                    // cursor.getFrame(0).points.get(1).z = newPt.z;
+                } catch (Exception e) {
+                }
+            }
+            for(Integer bolt : torem){
+                Driver.models.remove(bolt);
             }
             //space
             if(Driver.inp.keys[32] && boltCooldown < 1){
@@ -193,11 +209,11 @@ public class Fly {
                 LinkedList<Point3D> points2 = new LinkedList<>();
                 LinkedList<Integer[]> lines2 = new LinkedList<>();
                 LinkedList<Point2D[]> faces2 = new LinkedList<>();
-                Point3D c5 = c1.clone();
-                Point3D c6 = c2.clone();
+                Point3D c5 = shipM.getFrame(0).points.get(c1i).clone();
+                Point3D c6 = shipM.getFrame(0).points.get(c2i).clone();
                 if(side > 0){
-                    c5 = c3.clone();
-                    c6 = c4.clone();
+                    c5 = shipM.getFrame(0).points.get(c3i).clone();
+                    c6 = shipM.getFrame(0).points.get(c4i).clone();
                 }
                 c5.identifier = c5.hashCode();
                 c6.identifier = c6.hashCode();
